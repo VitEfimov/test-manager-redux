814432e6fd15b7e5af12183162a6ff6fed9b93df
VitEfimov <135293952+VitEfimov@users.noreply.github.com>
2024-04-22 18:34:35 -0400

    InfoIcon

---
+++ src/components/InfoIcon.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { TbInfoSquareFilled } from "react-icons/tb";
+
+export const InfoIcon = () => {
+  return (
+    <button>
+<TbInfoSquareFilled />
+
+    </button>
+  )
+}
