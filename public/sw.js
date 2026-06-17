c624dbfe7d5240a0dc7a60fb1644db47be686f93
VitEfimov <135293952+VitEfimov@users.noreply.github.com>
2026-06-17 16:52:48 -0400

    installer

---
+++ public/sw.js
@@ -0,0 +1,14 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('task-manager-store').then((cache) => cache.addAll([
+      '/',
+      '/index.html',
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
